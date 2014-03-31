873e101c1bd709c29836ee3700b6ae49dbf0b3ae
Thomas Järvstrand <thomas.jarvstrand@klarna.com>
2014-03-31 19:16:07 +0200

    Add dash dependency

--- edts-pkg.el
+++ edts-pkg.el
@@ -1,6 +1,7 @@
 (define-package "edts" "master" "Erlang Development Tool Suite"
   '((auto-complete         "20140322.321")
     (auto-highlight-symbol "1.55")
+    (dash                  "20140327.116")
     (eproject              "20140118.1608")
     (erlang                "20131025.6")
     (f                     "20140220.21")
